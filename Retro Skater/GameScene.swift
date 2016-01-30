87b980e7722159fd23768ed0f3f2a8eb88e9e83b
Rick C <antmathews@hotmail.com>
2016-01-30 19:10:03 +0100

    RETRO SKATE learning

--- Retro Skater/GameScene.swift
+++ Retro Skater/GameScene.swift
@@ -9,37 +9,61 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var backgroundsArray = [SKSpriteNode]()
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        makeBackground()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+       
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        print(backgroundsArray.count)
+    }
+    
+    
+    func makeBackground(){
+        var background = SKSpriteNode()
+        
+        //VALUES TO CHANGE POSITIONING
+        let BACKGROUND_BASE_VALUE: CGFloat = 60
+        let MID_BG_POS: CGFloat = 35
+        let FAR_BG_POS: CGFloat = 65
+        
+        //ITERATE THROUGH TO MAKE THE BACKGROUNDS AND APPEND TO ARRAY
+        for var i = 0; i < 3; i++ {
+            for var x = 0; x < 3; x++ {
+                background = SKSpriteNode(imageNamed: "bg\(i)")
+                background.anchorPoint = CGPointMake(0.5, 0)
+                background.zPosition = CGFloat(-i)
+                
+                
+                ////////////////////FIX THIS SECTION////////////////////
+                if backgroundsArray.count == 0{
+                    background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMinY(self.frame) + BACKGROUND_BASE_VALUE)
+                }else{
+                    background.position = CGPointMake(, CGRectGetMinY(self.frame) + BACKGROUND_BASE_VALUE)
+                }
+                
+                backgroundsArray.append(background)
+                self.addChild(background)
+            }
+        }
+        //POSTITION THEM IN PLACE
+        if backgroundsArray.count == 9 {
+            backgroundsArray[0].position = background.position
+            backgroundsArray[1].position = CGPoint(x: background.position.x, y: background.position.y + MID_BG_POS)
+            backgroundsArray[2].position = CGPoint(x: background.position.x, y: background.position.y + FAR_BG_POS)
+        }
+        
+        let moveBG = SKAction.moveByX(-50, y: 0, duration: 0.2)
+        let moveBGForever = SKAction.repeatActionForever(moveBG)
+        background.runAction(moveBGForever)
+        
     }
 }
